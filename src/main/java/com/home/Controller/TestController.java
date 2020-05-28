e73c4de6ef98e047e8140cd9e7c0040e33f56993
Mostafa Alsabagh <mostafa.alsabagh@datagearbi.com>
2020-05-28 18:35:54 +0200

    Add test mapping to test controller

--- src/main/java/com/home/Controller/TestController.java
+++ src/main/java/com/home/Controller/TestController.java
@@ -1,4 +1,13 @@
 package com.home.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @RequestMapping(value = "/testkmg")
+    public String welcomeTest(){
+        return "welcome kmg";
+    }
 }
