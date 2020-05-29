fa3b07e38b9df0282263cf7faafdf66bf56c68d6
Mostafa Alsabagh <mostafa.alsabagh@datagearbi.com>
2020-05-29 05:59:26 +0200

    Create custom service for our user loading by name.

---
+++ src/main/java/com/home/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.home.security;
+
+import com.home.Entity.AppUser;
+import com.home.Repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("KMG == username before : "+username);
+        Optional<AppUser> appUser = appUserRepository.findByUserName(username);
+        System.out.println("KMG == username : "+ appUser.get().getUserName());
+        appUser.orElseThrow(() -> new UsernameNotFoundException("User name not found !"+ username));
+        return appUser.map(CustomUserDetails::new).get();
+    }
+}
