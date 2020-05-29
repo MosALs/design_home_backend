d802a06e7966e087afedfc48e80412dcb01bdccb
Mostafa Alsabagh <mostafa.alsabagh@datagearbi.com>
2020-05-29 04:40:09 +0200

    Add CustomUserDetails Class to map our entityAppUser.

---
+++ src/main/java/com/home/security/CustomUserDetails.java
@@ -0,0 +1,63 @@
+package com.home.security;
+
+import com.home.Entity.AppUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+    private boolean active ;
+    private List<GrantedAuthority> roles;
+
+    public CustomUserDetails(AppUser appUser){
+        this.username = appUser.getUserName();
+        this.password = appUser.getPassword();
+        this.active = appUser.isActive();
+        this.roles = Arrays.stream(appUser.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
