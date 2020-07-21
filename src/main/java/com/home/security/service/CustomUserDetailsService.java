76bc517a289df82f2f32c50c5d296f85e6b132b0
Mostafa Alsabagh <mostafa.alsabagh@datagearbi.com>
2020-07-21 20:12:01 +0200

    Add get user role logic.

--- src/main/java/com/home/security/service/CustomUserDetailsService.java
+++ src/main/java/com/home/security/service/CustomUserDetailsService.java
@@ -2,11 +2,14 @@ package com.home.security.service;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.home.entities.AppUserEntity;
+import com.home.entities.UserRoleEntity;
 import com.home.jsonfilter.View;
 import com.home.repositories.AppUserRepository;
+import com.home.repositories.UserRoleRepository;
 import com.home.security.model.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,18 +23,26 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired()
     private AppUserRepository appUserRepository;
 
+    @Autowired()
+    private UserRoleRepository userRoleRepository;
+
+
     @JsonView(View.AuthenticateInfo.class)
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("KMG == loadUserByUsername() username before : "+username);
+    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("KMG == loadUserByUsername() username before : " + username);
         Optional<AppUserEntity> appUser = appUserRepository.findByUserName(username);
 
-        if (!appUser.isPresent()){
+        if (!appUser.isPresent()) {
             // if returned user is null
             return null;
         }
+        Optional<UserRoleEntity> userRoleEntity = userRoleRepository.findById(appUser.get().getUserRoleId());
+        String userRoleName = userRoleEntity.get().getUserRoleName();
+        SimpleGrantedAuthority role = new SimpleGrantedAuthority(userRoleName);
         // app user is not null.
         CustomUserDetails customUserDetails = appUser.map(CustomUserDetails::new).get();
+        customUserDetails.setRole(role);
 //        appUser.orElseThrow(() -> new UsernameNotFoundException("User name not found !"+ username));
         return customUserDetails;
     }
